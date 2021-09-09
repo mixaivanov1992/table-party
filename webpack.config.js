d3a98daf8405a5ec8ba00ea349222e5b103fa643
mikhail <mixaivanov1992@gmail.com>
2021-09-09 23:27:17 +0600

    настройка приложения

---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const filename = (ext) => isDev?`./[name].${ext}`:`./[name].[hash].${ext}`;
+const plugins = () => {
+    const base = [
+        new HTMLWebpackPlugin({
+            template: '../public/index.html',
+            minify: {
+                collapseWhitespace: !isDev
+            }
+        }),
+        new CleanWebpackPlugin(),
+    ]
+    if(!isDev) {
+        base.push(new BundleAnalyzerPlugin());
+    }
+    return base;
+}
+
+
+module.exports = {
+    mode: isDev?'development':'production',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+        main: ['@babel/polyfill', './index.tsx'],
+    }, 			                        
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, './'),
+            '@components': path.resolve(__dirname, './components'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        }
+    },
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,                                  
+                use: ['file-loader'],
+            },
+            {
+                test: /\.ts(x?)$/,                                  
+                use: ['ts-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ],
+                    }
+                }
+            },
+            {
+                test: /\.tsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-typescript',
+                            '@babel/preset-react'
+                        ],
+                    }
+                }
+            },
+        ]
+    },
+}
