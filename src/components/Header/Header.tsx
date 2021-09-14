d4ef457b14cf697ad6b44850c66d61583be9b45b
mikhail <mixaivanov1992@gmail.com>
2021-09-14 22:31:20 +0600

    Настройка подключения стилей

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from '@css/Header.module.css';
+
 interface Props {
 }
 
 const Header: React.FC<Props> = (props) => {
     return (
-        <div>1</div>
+        <div className={styles.color}>
+            <div>
+                <input placeholder="Поиск"/>
+            </div>
+            <div></div>
+            <div>
+                <button>Вход</button>
+            </div>
+        </div>
     );
 }
 
